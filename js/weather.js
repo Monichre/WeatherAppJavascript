bc7099853fe43813d5eeb6fe835dc61ab846589d
Liam Ellis <liamhellis@gmail.com>
2016-05-24 21:01:02 -0700

    backend up, gulpfile set up, bower running with front end dependencies, would be good basic template, have left out concat task, since it doesnt seem neccessary at this point

---
+++ js/weather.js
@@ -0,0 +1,13 @@
+var apiKey = "0e97f57887e6f68af87dca9e53de89a0";
+
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
